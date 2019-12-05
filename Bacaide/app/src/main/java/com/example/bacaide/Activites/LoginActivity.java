75833e12e0caf95b8773ddc0c7f978057a1c42e0
Rémi Bouvier <remibouvier99@gmail.com>
2019-12-05 20:10:40 +0100

    Création LoginActivity

---
+++ Bacaide/app/src/main/java/com/example/bacaide/Activites/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.bacaide.Activites;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.bacaide.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
