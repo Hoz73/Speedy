e15d727d5a0ffa987bb6e2705bb9fcf71719721b
Rémi Bouvier <remibouvier99@gmail.com>
2019-12-05 22:56:27 +0100

    ajout onClickListener sur LoginActivity

--- Bacaide/app/src/main/java/com/example/bacaide/Activites/LoginActivity.java
+++ Bacaide/app/src/main/java/com/example/bacaide/Activites/LoginActivity.java
@@ -2,15 +2,47 @@ package com.example.bacaide.Activites;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.bacaide.R;
 
 public class LoginActivity extends AppCompatActivity {
+    private Button loginButton, subButton, guestButton;
+    private EditText email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //Récupération des composants
+        loginButton = findViewById(R.id.connexionButton);
+        subButton = findViewById(R.id.inscriptionButton);
+        guestButton = findViewById(R.id.guestButton);
+        email = findViewById(R.id.email);
+        password =findViewById(R.id.password);
+
+        loginButton.setOnClickListener ((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        }
+        ));
+
+        subButton.setOnClickListener ((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SubActivity.class);
+                startActivity(intent);
+            }
+        }
+        ));
     }
 }
